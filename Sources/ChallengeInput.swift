e5e1594a3069ac1d7f3baf83cb7fc5db64a3f687
Frank Schlegel <frank.schlegel@digitalmasterpieces.com>
2021-12-02 07:51:26 +0100

    Added caching for input data.

--- Sources/ChallengeInput.swift
+++ Sources/ChallengeInput.swift
@@ -7,15 +7,25 @@ struct ChallengeInput {
     let data: Data
 
     init(year: UInt, day: UInt8, session: String) async throws {
-        let url = URL(string: "https://adventofcode.com/\(year)/day/\(day)/input")!
+        let fileManager = FileManager.default
+        // check if we have already downloaded the data
+        let cachedFilePath = fileManager.temporaryDirectory.appendingPathComponent("AoC_\(year)_\(day)_\(session).input")
+        if fileManager.fileExists(atPath: cachedFilePath.relativePath) {
+            self.data = fileManager.contents(atPath: cachedFilePath.relativePath)!
+        // if not, download and cache the data
+        } else {
+            let url = URL(string: "https://adventofcode.com/\(year)/day/\(day)/input")!
 
-        var request = URLRequest(url: url)
-        request.addValue("session=\(session)", forHTTPHeaderField: "Cookie")
-        let (data, response) = try await URLSession.shared.data(for: request)
-        if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode != 200 {
-            throw URLError(URLError.Code(rawValue: httpResponse.statusCode))
+            var request = URLRequest(url: url)
+            request.addValue("session=\(session)", forHTTPHeaderField: "Cookie")
+            let (data, response) = try await URLSession.shared.data(for: request)
+            if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode != 200 {
+                throw URLError(URLError.Code(rawValue: httpResponse.statusCode))
+            }
+
+            try data.write(to: cachedFilePath)
+            self.data = data
         }
-        self.data = data
     }
 
     func asString() -> String? {
