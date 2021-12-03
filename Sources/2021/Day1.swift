a866bc1404dcd66ad42fcbafee1fd07a364eae3b
Frank Schlegel <frank.schlegel@digitalmasterpieces.com>
2021-12-03 07:56:25 +0100

    Refactored day 1 to use Algorithms.

--- Sources/2021/Day1.swift
+++ Sources/2021/Day1.swift
@@ -1,14 +1,21 @@
+import Algorithms
+
+
 extension Challenges2021 {
 
     static func runDay1(input: ChallengeInput) async throws {
         let lines = input.lines()!.compactMap(UInt.init)
-        let numDeclines1 = zip(lines.dropLast(), lines.dropFirst()).filter({ $0 < $1 }).count
+
+        let numDeclines1 = self.countSlopes(in: lines, windowSize: 1)
         print("Part 1: There are \(numDeclines1) declining slopes ahead.")
 
-        let windowSize = 3
-        let windowSums = lines.dropLast(windowSize - 1).indices.map({ lines[$0...($0 + windowSize - 1)].reduce(0, +) })
-        let numDeclines2 = zip(windowSums.dropLast(), windowSums.dropFirst()).filter({ $0 < $1 }).count
+        let numDeclines2 = self.countSlopes(in: lines, windowSize: 3)
         print("Part 2: Looks more like \(numDeclines2) declining slopes ahead.")
+    }
+
+    private static func countSlopes<C: Collection>(in lines: C, windowSize: Int) -> Int where C.Element == UInt {
+        let windowSums = lines.windows(ofCount: windowSize).map({ $0.reduce(0, +) })
+        return windowSums.adjacentPairs().filter({ $0 < $1 }).count
     }
 
 }
