a09884b4cc3a76c90bb8c224a722a26751055eab
Frank Schlegel <frank.schlegel@digitalmasterpieces.com>
2021-12-03 07:45:02 +0100

    Added the super useful Algorithms package…
    Mainly to make working with collections much easier.

--- Package.swift
+++ Package.swift
@@ -7,12 +7,14 @@ let package = Package(
     name: "AdventOfCode",
     platforms: [.macOS(.v12)],
     dependencies: [
+        .package(url: "https://github.com/apple/swift-algorithms", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
     ],
     targets: [
         .executableTarget(
             name: "AdventOfCode",
             dependencies: [
+                .product(name: "Algorithms", package: "swift-algorithms"),
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
             ],
             path: "Sources"
